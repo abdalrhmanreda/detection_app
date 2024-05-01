34969900ced5153f68aab807266f8016ea1842b0
abdalrahmanreda24 <abdulrahman.20377239@compit.aun.edu.eg>
2024-05-01 23:40:36 +0300

    - init route

--- lib/DetecttionApp.dart
+++ lib/DetecttionApp.dart
@@ -1,5 +1,4 @@
 import 'package:detection_app/config/routes/router.dart';
-import 'package:detection_app/config/routes/routes_path.dart';
 import 'package:detection_app/config/themes/themes.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
@@ -20,7 +19,7 @@ class DetectionApp extends StatelessWidget {
         localizationsDelegates: AppLocalizations.localizationsDelegates,
         supportedLocales: AppLocalizations.supportedLocales,
         theme: Style.lightTheme,
-        initialRoute: RoutePath.getStarted,
+        initialRoute: initRoute,
         onGenerateRoute: appRouter.generateRoute,
       ),
     );
